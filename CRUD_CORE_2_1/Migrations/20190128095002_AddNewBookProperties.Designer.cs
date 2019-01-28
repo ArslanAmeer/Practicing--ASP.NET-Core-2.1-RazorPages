e86dff0230d255aa725ebd9ccb6452705629671f
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-28 15:37:16 +0500

    📦👌 Udpate: Adding few more properties & Valdation

--- CRUD_CORE_2_1/Migrations/20190128095002_AddNewBookProperties.Designer.cs
+++ CRUD_CORE_2_1/Migrations/20190128095002_AddNewBookProperties.Designer.cs
@@ -3,14 +3,16 @@ using CRUD_CORE_2_1.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CRUD_CORE_2_1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190128095002_AddNewBookProperties")]
+    partial class AddNewBookProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CRUD_CORE_2_1/Migrations/20190128095002_AddNewBookProperties.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD_CORE_2_1.Migrations
+{
+    public partial class AddNewBookProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
